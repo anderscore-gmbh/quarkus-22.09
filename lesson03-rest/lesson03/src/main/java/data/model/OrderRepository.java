6ea9651f43a0af665f37cfc40c931deaf1295d44
Jan Luehr <jan.luehr@anderscore.com>
2020-10-16 15:27:03 +0200

    Lesson02: Solutions / export Lesson03: Skeleton

---
+++ lesson03-rest/lesson03/src/main/java/data/model/OrderRepository.java
@@ -0,0 +1,52 @@
+package data.model;
+
+import java.math.BigDecimal;
+import java.util.*;
+
+/**
+ * Einfaches, in-memory repository fuer Bestellungen
+ */
+public class OrderRepository {
+
+    private static Long nonce = 1L;
+
+    private static Map<Long,Order> db = new HashMap<>();
+    static {
+        Long id = generateID();
+        db.put(id, new Order(
+                id, new Date(), 42L, Status.LOST, Arrays.asList("Funghi"), new BigDecimal(6.5)
+        ));
+    }
+
+    // Aktualisieren oder anlegen
+    public void save(Order o){
+        if(o.getOrderId() == null){
+            o.setOrderId(generateID());
+        }
+        db.put(o.getOrderId(),o);
+    }
+
+    // Nach Id suchen
+    public Order findById(Long id){
+        return db.get(id);
+    }
+
+    // Alle Orders zurück geben
+    public Collection<Order> findAll() {
+        return db.values();
+    }
+
+    public void delete(Order o){
+        if(o.getOrderId() == null) {
+            throw new IllegalArgumentException("Not in Database");
+        } else {
+            db.remove(o);
+        }
+    }
+
+    // ID erzeugen
+    public static Long generateID(){
+        return ++nonce;
+    }
+
+}
---
+++ lesson03-rest/lesson03/src/main/java/net/gfu/quarkus/endpoints/OrderResource.java
@@ -0,0 +1,38 @@
+package net.gfu.quarkus.endpoints;
+
+import data.model.Order;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+public class OrderResource {
+
+    // CRUD repository
+
+    // Create
+    public void create(Order o){
+
+    }
+
+    // Read
+    public void read(Long id){
+
+    }
+
+    // Update
+    public void update(Order o){
+
+    }
+
+    // Delete
+    public void delete(Long id){
+
+    }
+
+    // Index
+    public void index(){
+
+    }
+}
