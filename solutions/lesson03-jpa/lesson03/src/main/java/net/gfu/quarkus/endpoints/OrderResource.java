68e9f318f0f2e06bcaf15f2b5560e485ac2978d9
Jan Lühr <jan.luehr@anderscore.com>
2020-10-22 13:10:04 +0200

    JPA, Panache

---
+++ solutions/lesson03-jpa/lesson03/src/main/java/net/gfu/quarkus/endpoints/OrderResource.java
@@ -0,0 +1,87 @@
+package net.gfu.quarkus.endpoints;
+
+import data.model.Order;
+import data.model.OrderRepository;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Path("orders")
+@ApplicationScoped
+@Produces("application/json")
+@Consumes("application/json")
+public class OrderResource {
+
+    // CRUD repository
+    @Inject
+    private OrderRepository repository;
+
+    // Create
+    @POST
+    public Response create(Order o, @Context UriInfo uriInfo){
+        if(o == null) {
+            return Response.status(402).build();
+        }
+        if(o.getOrderId() != null) {
+            return Response.status(422).build();
+        }
+        this.repository.save(o);
+
+        URI newUri;
+        try {
+            String path = uriInfo.getAbsolutePath().toString() + "/" + o.getOrderId();
+            newUri = new URI(path);
+        } catch (URISyntaxException e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        }
+        return Response.created(newUri).build();
+    }
+
+    // Read
+    @GET
+    @Path("{id}")
+    public Response read(@PathParam("id") Long id){
+        Order order = this.repository.findById(id);
+        if(order == null){
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+        return Response.ok(order).build();
+    }
+
+    // Update
+    @PATCH
+    public Response update(Order o){
+        if(o.getOrderId() == null || this.repository.findById(o.getOrderId()) == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+        this.repository.save(o);
+        return Response.noContent().build();
+    }
+
+    // Delete
+    @DELETE
+    @Path("{id}")
+    public Response delete(@PathParam("id") Long id){
+        Order o = this.repository.findById(id);
+        if(o == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+        this.repository.delete(o);
+        return Response.noContent().build();
+    }
+
+    // Index
+    @GET
+    public List<Order> index(){
+        return new ArrayList<>(this.repository.findAll());
+    }
+}
