9d4c388c716b1903940f43c84fd70072852c2f19
Jan Luehr <jan.luehr@anderscore.com>
2020-10-25 20:17:43 +0100

    Added generated slides

---
+++ presentation/target/generated-slides/sources/lessen01-intro-business-service.java
@@ -0,0 +1,8 @@
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class GreetingService {
+    public String greeting(String name) {
+        return "hello " + name;
+    }
+}
