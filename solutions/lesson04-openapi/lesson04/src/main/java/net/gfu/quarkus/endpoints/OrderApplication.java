debd191055200c12d2648e3691ae86cbe863a70e
Jan Lühr <jan.luehr@anderscore.com>
2020-10-28 09:34:49 +0100

    Fix: OpenAPI solution

---
+++ solutions/lesson04-openapi/lesson04/src/main/java/net/gfu/quarkus/endpoints/OrderApplication.java
@@ -0,0 +1,35 @@
+package net.gfu.quarkus.endpoints;
+
+import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
+import org.eclipse.microprofile.openapi.annotations.info.Contact;
+import org.eclipse.microprofile.openapi.annotations.info.Info;
+import org.eclipse.microprofile.openapi.annotations.info.License;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Application;
+
+
+
+@Path("orders")
+@Produces("application/json")
+@Consumes("application/json")
+@OpenAPIDefinition(
+        tags = {
+                @Tag(name="order", description="Operationen zur Bestellung"),
+        },
+        info = @Info(
+                title="Order API",
+                version = "0.0.1",
+                contact = @Contact(
+                        name = "Jan Luehr",
+                        url = "https://anderscore.com",
+                        email = "jan.luehr@anderscore.com"),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "http://www.apache.org/licenses/LICENSE-2.0.html"))
+)
+public class OrderApplication extends Application {
+}
--- solutions/lesson04-openapi/lesson04/src/main/java/net/gfu/quarkus/endpoints/OrderResource.java
+++ solutions/lesson04-openapi/lesson04/src/main/java/net/gfu/quarkus/endpoints/OrderResource.java
@@ -2,21 +2,10 @@ package net.gfu.quarkus.endpoints;
 
 import data.model.Order;
 import data.model.OrderRepository;
-import io.quarkus.runtime.annotations.RegisterForReflection;
-import org.eclipse.microprofile.metrics.MetricUnits;
-import org.eclipse.microprofile.metrics.annotation.Counted;
-import org.eclipse.microprofile.metrics.annotation.Gauge;
-import org.eclipse.microprofile.metrics.annotation.Timed;
-import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
 import org.eclipse.microprofile.openapi.annotations.Operation;
-import org.eclipse.microprofile.openapi.annotations.info.Contact;
-import org.eclipse.microprofile.openapi.annotations.info.Info;
-import org.eclipse.microprofile.openapi.annotations.info.License;
 import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
-import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
-import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.ws.rs.*;
@@ -25,26 +14,7 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.List;
 
-@Path("orders")
-@Produces("application/json")
-@Consumes("application/json")
-@OpenAPIDefinition(
-        tags = {
-                @Tag(name="order", description="Operationen zur Bestellung"),
-        },
-        info = @Info(
-                title="Order API",
-                version = "0.0.1",
-                contact = @Contact(
-                        name = "Jan Luehr",
-                        url = "https://anderscore.com",
-                        email = "jan.luehr@anderscore.com"),
-                license = @License(
-                        name = "Apache 2.0",
-                        url = "http://www.apache.org/licenses/LICENSE-2.0.html"))
-)
 public class OrderResource {
 
     // CRUD repository
