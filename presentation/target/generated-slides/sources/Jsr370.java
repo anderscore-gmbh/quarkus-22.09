0e6557dee0c1ef900b7dd87faf517b7c2d19523f
Daniel Krämer <daniel.kraemer@anderscore.com>
2021-01-15 21:55:51 +0100

    Updated slides

--- presentation/target/generated-slides/sources/Jsr370.java
+++
@@ -1,9 +0,0 @@
-@Path("/echo")
-public class EchoResource {
-    // ...
-    @GET @Path("{spell}")
-    @Produces(MediaType.TEXT_PLAIN)
-    public String echo(@PathParam String spell) {
-        return service.echo(spell);
-    }
-}
--- presentation/target/generated-slides/sources/lessen01-intro-business-service.java
+++
@@ -1,8 +0,0 @@
-import javax.enterprise.context.ApplicationScoped;
-
-@ApplicationScoped
-public class GreetingService {
-    public String greeting(String name) {
-        return "hello " + name;
-    }
-}
