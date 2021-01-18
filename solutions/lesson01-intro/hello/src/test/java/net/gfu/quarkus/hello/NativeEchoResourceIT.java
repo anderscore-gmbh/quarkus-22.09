1346ca9d9ff6ddcefa0f64c8f5292be30b1880df
Daniel Krämer <daniel.kraemer@anderscore.com>
2021-01-18 16:33:24 +0100

    Added some CDI slides, fixed introductory examples

--- solutions/lesson01-intro/hello/src/test/java/net/gfu/quarkus/hello/NativeEchoResourceIT.java
+++ solutions/lesson01-intro/hello/src/test/java/net/gfu/quarkus/hello/NativeEchoResourceIT.java
@@ -3,7 +3,7 @@ package net.gfu.quarkus.hello;
 import io.quarkus.test.junit.NativeImageTest;
 
 @NativeImageTest
-public class NativeGreetingResourceIT extends GreetingResourceTest {
+public class NativeEchoResourceIT extends EchoResourceTest {
 
     // Execute the same tests but in native mode.
 }
