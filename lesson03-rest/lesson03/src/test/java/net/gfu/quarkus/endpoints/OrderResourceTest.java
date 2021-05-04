a51ec17647a0ec80b56ca89e9c15568013ea2723
Andre Lambertz <andre.lambertz@anderscore.com>
2021-05-04 09:57:16 +0000

    Update OrderResourceTest.java
    added tests

--- lesson03-rest/lesson03/src/test/java/net/gfu/quarkus/endpoints/OrderResourceTest.java
+++ lesson03-rest/lesson03/src/test/java/net/gfu/quarkus/endpoints/OrderResourceTest.java
@@ -1,19 +1,56 @@
 package net.gfu.quarkus.endpoints;
 
+import data.model.Status;
+import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
+import javax.json.Json;
+import javax.json.JsonObject;
+import java.time.ZonedDateTime;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 @QuarkusTest
+@TestHTTPEndpoint(OrderResource.class)
 public class OrderResourceTest {
 
-
     @Test
-    public void testHelloEndpoint() {
-        assertThat("Nothing", true);
+    public void testNoOrder4711(){
+        given()
+                .when().get("4711")
+                .then().body(is(emptyString()));
     }
 
+    @Test
+    public void testHasOrder1(){
+        // Test
+        given()
+                .when().get("1")
+                .then().statusCode(200)
+                .body("status", equalTo("LOST"));
+    }
+
+    @Test
+    public void testCreateOrder(){
+        // Creating JSON
+        JsonObject obj = Json.createObjectBuilder()
+                .add("orderDateTime", ZonedDateTime.now().toString())
+                .add("customerId", 42L)
+                .add("status", Status.LOST.toString())
+                .add("pizzaList",Json.createArrayBuilder()
+                        .add(Json.createObjectBuilder()
+                                .add("name", "Funghi")
+                                .add("price", "6.5"))
+                        .build())
+                .add("totalPrize", "6.5").build();
+
+        // Test
+        given().contentType("application/json")
+                .body(obj)
+                .when().post("/")
+                .then().body(is(emptyString()));
+    }
 }
