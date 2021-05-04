52b35f2567a53a2340f4490555afcb3e726a60c7
Andre Lambertz <andre.lambertz@anderscore.com>
2021-05-04 13:38:07 +0000

    Update OrderResourceTest.java
    tests are now functional

--- solutions/lesson04-checks/lesson04/src/test/java/net/gfu/quarkus/endpoints/OrderResourceTest.java
+++ solutions/lesson04-checks/lesson04/src/test/java/net/gfu/quarkus/endpoints/OrderResourceTest.java
@@ -1,15 +1,13 @@
 package net.gfu.quarkus.endpoints;
 
-import data.model.Order;
+import data.model.Status;
 import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
 import javax.json.Json;
 import javax.json.JsonObject;
-import java.math.BigDecimal;
-import java.util.Arrays;
-import java.util.Date;
+import java.time.ZonedDateTime;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
@@ -19,22 +17,34 @@ import static org.hamcrest.Matchers.*;
 @TestHTTPEndpoint(OrderResource.class)
 public class OrderResourceTest {
 
-
     @Test
     public void testNoOrder4711(){
         given()
-            .when().get("4711")
-            .then().statusCode(404);
+                .when().get("4711")
+                .then().statusCode(404);
     }
 
     @Test
     public void testHasOrder1(){
+        // Creating JSON-Order Object
+        JsonObject obj = Json.createObjectBuilder()
+                .add("orderDateTime", ZonedDateTime.now().toString())
+                .add("customerId", 42L)
+                .add("status", Status.LOST.toString())
+                .add("pizzaList",Json.createArrayBuilder()
+                        .add(Json.createObjectBuilder()
+                                .add("name", "Funghi")
+                                .add("price", "6.5"))
+                        .build())
+                .add("totalPrize", "6.5").build();
+        given().contentType("application/json").body(obj).when().post("/");
+
+        // Test
         given()
                 .when().get("1")
                 .then().statusCode(200)
                 .body("status", equalTo("LOST"));
     }
-
 
     @Test
     public void testCannotCreateOrderWithGivenID(){
@@ -49,15 +59,22 @@ public class OrderResourceTest {
 
     @Test
     public void testCreateOrder(){
+        // Creating JSON
         JsonObject obj = Json.createObjectBuilder()
+                .add("orderDateTime", ZonedDateTime.now().toString())
                 .add("customerId", 42L)
-                .add("pizzaList",Json.createArrayBuilder().add("Funghi").build())
-                .add("totalPrize", "7").build();
+                .add("status", Status.LOST.toString())
+                .add("pizzaList",Json.createArrayBuilder()
+                        .add(Json.createObjectBuilder()
+                                .add("name", "Funghi")
+                                .add("price", "6.5"))
+                        .build())
+                .add("totalPrize", "6.5").build();
+
+        // Test
         given().contentType("application/json")
                 .body(obj)
                 .when().post("/")
                 .then().statusCode(201).body(is(emptyString()));
-
-
     }
 }
