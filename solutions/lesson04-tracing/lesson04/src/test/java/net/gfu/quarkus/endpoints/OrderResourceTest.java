a3273df8674c6149dd721c1f84c73d175595d19e
André Marcel Lambertz <andre.lambertz@alumni.fh-aachen.de>
2021-05-06 11:39:02 +0200

    update solutions/lessons04-tracing/lesson04
    better date management with java.time
    
    tests are now functional

--- solutions/lesson04-tracing/lesson04/src/test/java/net/gfu/quarkus/endpoints/OrderResourceTest.java
+++ solutions/lesson04-tracing/lesson04/src/test/java/net/gfu/quarkus/endpoints/OrderResourceTest.java
@@ -1,15 +1,14 @@
 package net.gfu.quarkus.endpoints;
 
-import data.model.Order;
+import data.model.Status;
 import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.mapper.ObjectMapperType;
 import org.junit.jupiter.api.Test;
 
 import javax.json.Json;
 import javax.json.JsonObject;
-import java.math.BigDecimal;
-import java.util.Arrays;
-import java.util.Date;
+import java.time.ZonedDateTime;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
@@ -19,22 +18,35 @@ import static org.hamcrest.Matchers.*;
 @TestHTTPEndpoint(OrderResource.class)
 public class OrderResourceTest {
 
-
     @Test
     public void testNoOrder4711(){
         given()
-            .when().get("4711")
-            .then().statusCode(404);
+                .when().get("4711")
+                .then().statusCode(404);
     }
 
     @Test
     public void testHasOrder1(){
+        // Creating JSON-Order Object
+        JsonObject obj = Json.createObjectBuilder()
+                .add("orderDateTime", ZonedDateTime.now().toString())
+                .add("customerId", 42L)
+                .add("status", Status.LOST.toString())
+                .add("pizzaList",Json.createArrayBuilder()
+                        .add(Json.createObjectBuilder()
+                                .add("name", "Funghi")
+                                .add("price", "6.5"))
+                        .build())
+                .add("totalPrize", "6.5").build();
+        given().contentType("application/json")
+                .body(obj, ObjectMapperType.JSONB).when().post("/");
+
+        // Test
         given()
                 .when().get("1")
                 .then().statusCode(200)
                 .body("status", equalTo("LOST"));
     }
-
 
     @Test
     public void testCannotCreateOrderWithGivenID(){
@@ -42,22 +54,29 @@ public class OrderResourceTest {
                 .add("customerId", 42L)
                 .add("orderId",42L).build();
         given().contentType("application/json")
-                .body(obj)
+                .body(obj, ObjectMapperType.JSONB)
                 .when().post("/")
                 .then().statusCode(422).body(is(emptyString()));
     }
 
     @Test
     public void testCreateOrder(){
+        // Creating JSON
         JsonObject obj = Json.createObjectBuilder()
+                .add("orderDateTime", ZonedDateTime.now().toString())
                 .add("customerId", 42L)
-                .add("pizzaList",Json.createArrayBuilder().add("Funghi").build())
-                .add("totalPrize", "7").build();
+                .add("status", Status.LOST.toString())
+                .add("pizzaList",Json.createArrayBuilder()
+                        .add(Json.createObjectBuilder()
+                                .add("name", "Funghi")
+                                .add("price", "6.5"))
+                        .build())
+                .add("totalPrize", "6.5").build();
+
+        // Test
         given().contentType("application/json")
-                .body(obj)
+                .body(obj, ObjectMapperType.JSONB)
                 .when().post("/")
                 .then().statusCode(201).body(is(emptyString()));
-
-
     }
 }
