e0a2d5dd5bbb0161a0fa7d735568d5d6cb3e3b42
André Marcel Lambertz <andre.lambertz@alumni.fh-aachen.de>
2021-05-17 09:57:23 +0200

    Quarkus Test funktionieren noch nicht für security

--- solutions/lesson05-security/src/test/java/net/gfu/quarkus/endpoints/OrderResourceTest.java
+++ solutions/lesson05-security/src/test/java/net/gfu/quarkus/endpoints/OrderResourceTest.java
@@ -18,66 +18,66 @@ import static org.hamcrest.Matchers.*;
 @TestHTTPEndpoint(OrderResource.class)
 public class OrderResourceTest {
 
-//    @Test
-//    public void testNoOrder4711(){
-//        given()
-//                .auth().basic("scott", "tiger")
-//                .when().get("4711")
-//                .then().statusCode(404);
-//    }
-//
-//    @Test
-//    public void testHasOrder1(){
-//        // Creating JSON-Order Object
-//        JsonObject obj = Json.createObjectBuilder()
-//                .add("orderDateTime", ZonedDateTime.now().toString())
-//                .add("customerId", 42L)
-//                .add("status", Status.LOST.toString())
-//                .add("pizzaList",Json.createArrayBuilder()
-//                        .add(Json.createObjectBuilder()
-//                                .add("name", "Funghi")
-//                                .add("price", "6.5"))
-//                        .build())
-//                .add("totalPrize", "6.5").build();
-//        given().contentType("application/json")
-//                .body(obj, ObjectMapperType.JSONB).when().post("/");
-//
-//        // Test
-//        given()
-//                .when().get("1")
-//                .then().statusCode(200)
-//                .body("status", equalTo("LOST"));
-//    }
-//
-//    @Test
-//    public void testCannotCreateOrderWithGivenID(){
-//        JsonObject obj = Json.createObjectBuilder()
-//                .add("customerId", 42L)
-//                .add("orderId",42L).build();
-//        given().contentType("application/json")
-//                .body(obj, ObjectMapperType.JSONB)
-//                .when().post("/")
-//                .then().statusCode(422).body(is(emptyString()));
-//    }
-//
-//    @Test
-//    public void testCreateOrder(){
-//        // Creating JSON
-//        JsonObject obj = Json.createObjectBuilder()
-//                .add("orderDateTime", ZonedDateTime.now().toString())
-//                .add("customerId", 42L)
-//                .add("status", Status.LOST.toString())
-//                .add("pizzaList",Json.createArrayBuilder()
-//                        .add(Json.createObjectBuilder()
-//                                .add("name", "Funghi")
-//                                .add("price", "6.5"))
-//                        .build())
-//                .add("totalPrize", "6.5").build();
-//
-//        // Test
-//        given().contentType("application/json")
-//                .body(obj, ObjectMapperType.JSONB)
-//                .when().post("/")
-//                .then().statusCode(201).body(is(emptyString()));
-//    }
+    @Test
+    public void testNoOrder4711(){
+        given()
+                .auth().basic("scott", "tiger")
+                .when().get("4711")
+                .then().statusCode(404);
+    }
+
+    @Test
+    public void testHasOrder1(){
+        // Creating JSON-Order Object
+        JsonObject obj = Json.createObjectBuilder()
+                .add("orderDateTime", ZonedDateTime.now().toString())
+                .add("customerId", 42L)
+                .add("status", Status.LOST.toString())
+                .add("pizzaList",Json.createArrayBuilder()
+                        .add(Json.createObjectBuilder()
+                                .add("name", "Funghi")
+                                .add("price", "6.5"))
+                        .build())
+                .add("totalPrize", "6.5").build();
+        given().contentType("application/json")
+                .body(obj, ObjectMapperType.JSONB).when().post("/");
+
+        // Test
+        given()
+                .when().get("1")
+                .then().statusCode(200)
+                .body("status", equalTo("LOST"));
+    }
+
+    @Test
+    public void testCannotCreateOrderWithGivenID(){
+        JsonObject obj = Json.createObjectBuilder()
+                .add("customerId", 42L)
+                .add("orderId",42L).build();
+        given().contentType("application/json")
+                .body(obj, ObjectMapperType.JSONB)
+                .when().post("/")
+                .then().statusCode(422).body(is(emptyString()));
+    }
+
+    @Test
+    public void testCreateOrder(){
+        // Creating JSON
+        JsonObject obj = Json.createObjectBuilder()
+                .add("orderDateTime", ZonedDateTime.now().toString())
+                .add("customerId", 42L)
+                .add("status", Status.LOST.toString())
+                .add("pizzaList",Json.createArrayBuilder()
+                        .add(Json.createObjectBuilder()
+                                .add("name", "Funghi")
+                                .add("price", "6.5"))
+                        .build())
+                .add("totalPrize", "6.5").build();
+
+        // Test
+        given().contentType("application/json")
+                .body(obj, ObjectMapperType.JSONB)
+                .when().post("/")
+                .then().statusCode(201).body(is(emptyString()));
+    }
 }
